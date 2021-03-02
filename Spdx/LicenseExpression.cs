9d66c06a0ae40b473018b198bcee2c72c3743bec
LizeLive <github@lize.live>
2021-03-01 20:50:09 -0800

    add license expression

--- Spdx/LicenseExpression.cs
+++ Spdx/LicenseExpression.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using static Spdx.Parser;
 
 namespace Spdx
 {
-    public class LicenseExpression : IExpr
+    public class LicenseExpression
     {
         public LicenseExpression Left { get; set; }
         public LicenseExpression Right { get; set; }
@@ -14,14 +13,14 @@ namespace Spdx
         public LicenseException Exception { get; set; }
         public License License { get; set; }
 
-        public bool Equals(IExpr other)
+        public override bool Equals(object obj)
         {
             return obj is LicenseExpression expression &&
-          EqualityComparer<LicenseExpression>.Default.Equals(Left, expression.Left) &&
-          EqualityComparer<LicenseExpression>.Default.Equals(Right, expression.Right) &&
-          Conjunction == expression.Conjunction &&
-          EqualityComparer<LicenseException>.Default.Equals(Exception, expression.Exception) &&
-          EqualityComparer<License>.Default.Equals(License, expression.License);
+                   EqualityComparer<LicenseExpression>.Default.Equals(Left, expression.Left) &&
+                   EqualityComparer<LicenseExpression>.Default.Equals(Right, expression.Right) &&
+                   Conjunction == expression.Conjunction &&
+                   EqualityComparer<LicenseException>.Default.Equals(Exception, expression.Exception) &&
+                   EqualityComparer<License>.Default.Equals(License, expression.License);
         }
     }
 
