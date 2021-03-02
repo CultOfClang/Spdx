11dd7e3e944e431f90f344b959bbe98a835c3ca5
LizeLive <github@lize.live>
2021-03-01 20:50:42 -0800

    add parser

---
+++ Spdx/Parser.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Pidgin;
+using Pidgin.Expression;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+using static Spdx.ParserUtils;
+namespace Spdx
+{
+
+    public class Parser
+    {
+        //public Parser<char, string> Parser;
+        public Parser()
+        {
+            var and = Binary(Tok("AND").ThenReturn(Conjunction.And));
+            var with = Binary(Tok("WITH").ThenReturn(Conjunction.With));
+            var or = Binary(Tok("OR").ThenReturn(Conjunction.With));
+
+
+            var idstring = Tok(OneOf(LetterOrDigit, Char('-'), Char('.')).AtLeastOnceString());
+            
+            
+            var licenseId = idstring;
+            var licenseExceptionId = idstring;
+            var documentRef = String("DocumentRef-").ThenReturn(idstring);
+
+            var licenseRef = documentRef.Then(Char(':')).Optional().Then(String("LicenseRef-").ThenReturn(idstring)).Select(x => "helpme");
+            var simpleExpression = OneOf(
+                licenseId,
+                licenseId.Then(String("+")),
+                licenseRef
+                );
+
+        }
+    }
+
+
+    public static class ExprParser
+    {
+
+
+
+
+        private static readonly Parser<char, Func<IExpr, IExpr, IExpr>> Add
+            = Binary(Tok("+").ThenReturn(BinaryOperatorType.Add));
+        private static readonly Parser<char, Func<IExpr, IExpr, IExpr>> Mul
+            = Binary(Tok("*").ThenReturn(BinaryOperatorType.Mul));
+        private static readonly Parser<char, Func<IExpr, IExpr>> Neg
+            = Unary(Tok("-").ThenReturn(UnaryOperatorType.Neg));
+        private static readonly Parser<char, Func<IExpr, IExpr>> Complement
+            = Unary(Tok("~").ThenReturn(UnaryOperatorType.Complement));
+
+        private static readonly Parser<char, IExpr> Identifier
+            = Tok(Letter.Then(LetterOrDigit.ManyString(), (h, t) => h + t))
+                .Select<IExpr>(name => new Identifier(name))
+                .Labelled("identifier");
+        private static readonly Parser<char, IExpr> Literal
+            = Tok(Num)
+                .Select<IExpr>(value => new Literal(value))
+                .Labelled("integer literal");
+
+        private static readonly Parser<char, IExpr> Expr = ExpressionParser.Build<char, IExpr>(
+            expr => (
+                OneOf(
+                    Identifier,
+                    Literal,
+                    Parenthesised(expr).Labelled("parenthesised expression")
+                ),
+                new[]
+                {
+                    Operator.Prefix(Neg).And(Operator.Prefix(Complement)),
+                    Operator.InfixL(Mul),
+                    Operator.InfixL(Add)
+                }
+            )
+        ).Labelled("expression");
+
+        public static IExpr ParseOrThrow(string input)
+            => Expr.ParseOrThrow(input);
+    }
+}
---
+++ Spdx/ParserUtils.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Pidgin;
+using Pidgin.Expression;
+using static Pidgin.Parser;
+
+namespace Spdx
+{
+
+    /// <summary>
+    /// From https://github.com/benjamin-hodgson/Pidgin/blob/master/Pidgin.Examples/Expression/
+    /// </summary>
+    public static class ParserUtils
+    {
+        public static Parser<char, Func<IExpr, IExpr, IExpr>> Binary<T>(Parser<char, T> op)
+    => op.Select<Func<IExpr, IExpr, IExpr>>(type => (l, r) => new BinaryOp<T>(type, l, r));
+        public static Parser<char, Func<IExpr, IExpr>> Unary(Parser<char, UnaryOperatorType> op)
+            => op.Select<Func<IExpr, IExpr>>(type => o => new UnaryOp(type, o));
+
+        public static Parser<char, T> Tok<T>(Parser<char, T> token)
+    => Try(token).Before(SkipWhitespaces);
+        public static Parser<char, string> Tok(string token)
+            => Tok(String(token));
+
+        public static Parser<char, T> Parenthesised<T>(Parser<char, T> parser)
+            => parser.Between(Tok("("), Tok(")"));
+        public interface IExpr : IEquatable<IExpr>
+        {
+        }
+        public class UnaryOp : IExpr
+        {
+            public UnaryOperatorType Type { get; }
+            public IExpr Expr { get; }
+
+            public UnaryOp(UnaryOperatorType type, IExpr expr)
+            {
+                Type = type;
+                Expr = expr;
+            }
+
+            public bool Equals(IExpr other)
+                => other is UnaryOp u
+                && this.Type == u.Type
+                && this.Expr.Equals(u.Expr);
+        }
+        public class BinaryOp<BinaryOperatorType> : IExpr
+        {
+            public BinaryOperatorType Type { get; }
+            public IExpr Left { get; }
+            public IExpr Right { get; }
+
+            public BinaryOp(BinaryOperatorType type, IExpr left, IExpr right)
+            {
+                Type = type;
+                Left = left;
+                Right = right;
+            }
+
+            public bool Equals(IExpr other)
+                => other is BinaryOp<BinaryOperatorType> b
+                && this.Type.Equals(b.Type)
+                && this.Left.Equals(b.Left)
+                && this.Right.Equals(b.Right);
+        }
+    }
+}
